738ea1ceff4d249565ddba21bb6fa292d70796ca
waldemarvogt <waldemar.vogt.wv@gmail.com>
2019-07-16 16:31:08 +0200

    init commit

---
+++ statystyka_opisowa2.R
@@ -0,0 +1,17 @@
+
+### Miary zmiennosci
+
+# git - system kontroli wersji
+
+# range
+
+zb1 <- c(1,2,3,4,5,6,6,7,8,9,10,11)
+zb2 <- c(4,5,5,5,6,6,6,6,7,7,8,8)
+
+install.packages("devtools")
+library(devtools)
+
+install_github('karabanb/BKmisc')
+library(BKmisc)
+
+BKmisc::start_proj()
